617a70bb860ff9855726bc727dba9e24926c852e
aulia14 <dota6008@gmail.com>
2018-02-16 17:48:56 +0700

    2.16.1

---
+++ Assets/Scripts/Bomb.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour {
+    private Scores myScore;
+    int scoreMin = 5;
+    void Start()
+    {
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        myScore = other.gameObject.GetComponent<Scores>();
+        Destroy(gameObject);
+        Scores.score -= 5;
+        if (Scores.score <= 0) {
+            Scores.score = 0;
+        }
+        
+    }
+}
---
+++ Assets/Scripts/ObjectDestroy.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectDestroy : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject, 0.1f);
+    }
+}
